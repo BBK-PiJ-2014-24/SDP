497c7b1933499c31a0abb7744714b5e71f3584ce
Stewart Newnham <git@newnham.com>
2016-02-07 10:05:58 +0800

    OutInstruction
    Implemented the Out Operation

---
+++ cw-one/src/sml/OutInstruction.java
@@ -0,0 +1,33 @@
+package sml;
+
+/**
+ * This class is for the Multiply Operator 
+ *
+ * @author someone
+ */
+
+public class OutInstruction extends Instruction {
+
+    private int op1;
+ 
+
+    public OutInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    public OutInstruction(String label, int op1) {
+        this(label, "out");
+        this.op1 = op1;
+    }
+
+    @Override
+    public void execute(Machine m) {
+        int value1 = m.getRegisters().getRegister(op1);
+        System.out.println(value1);  
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + op1;
+    }
+}

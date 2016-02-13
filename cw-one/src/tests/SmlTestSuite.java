b9c973dbdefa06cf3b26862b899d040cef49979f
Stewart Newnham <git@newnham.com>
2016-02-13 11:25:39 +0800

    smLTestSuite
    Added TestReflections() to test suite

--- cw-one/src/tests/SmlTestSuite.java
+++ cw-one/src/tests/SmlTestSuite.java
@@ -5,7 +5,18 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestInstruction_Add.class,  })
+@SuiteClasses({ TestInstruction_Add.class,
+				TestInstruction_Subtract.class,
+				TestInstruction_Multiply.class,
+				TestInstruction_Divide.class,
+				TestInstruction_Lin.class,
+				TestInstruction_Bnz.class,
+				TestReflections.class,
+				TestInstruction_Out.class
+				
+				
+				
+})
 public class SmlTestSuite {
 
 }
